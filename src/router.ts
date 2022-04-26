542a3657a63d89c98956d36a574a6241a4f9351f
Jochern <johannes.kling@outlook.de>
2022-04-26 16:02:34 +0200

    asd

--- src/router.ts
+++ src/router.ts
@@ -7,23 +7,7 @@ import {relaisOn,relaisOff} from './gpio/relais'
 let router = Router()
 
 router.get('/video', (req,res) => {         //important! client has to close connection 
-    res.status(200).set({
-        "connection": "keep-alive",
-        "content-type": "text/event-stream"
-    });
 
-    res.write('retry: 10000\n\n');          //tells client to retry every 10000 sec if connection lost
-    
-    req.on('close', () => {
-        console.log('close')
-        clearInterval(interval)
-    });
-    
-    let interval = setInterval(async ()=> {
-        let img = await (await captureImage()).toString('base64')
-        console.log('sending')
-        res.write(`data: ${img}\n\n`)
-    }, 3000) 
 })
 
 router.get('/moveDown', (req,res) => {
