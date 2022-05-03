1ac5cc18dce8c7bab48d1d435efeddead639a7b1
Jochern <johannes.kling@outlook.de>
2022-05-03 17:20:55 +0200

    as

--- src/router.ts
+++ src/router.ts
@@ -1,51 +1,55 @@
-import {Router} from 'express';
-import {captureImage} from '../src/video/capture'
+import { Router } from 'express';
+import videoStream from './video/capture';
 import moveDown from './gpio/moveDown';
 import moveUp from './gpio/moveUp';
-import {relaisOn,relaisOff} from './gpio/relais'
+import { relaisOn, relaisOff } from './gpio/relais'
 
 let router = Router()
 
-router.get('/video', (req,res) => {         //important! client has to close connection 
+videoStream.acceptConnections(router, {
+    width: 400,
+    height: 400,
+    fps: 7,
+    encoding: 'JPEG',
+    quality: 5 //lower is faster
+}, '/stream.mjpg', true)
 
-})
-
-router.get('/moveDown', (req,res) => {
+router.get('/moveDown', (req, res) => {
     moveDown()
-    .then((value) => {
-        console.log('resolved')
-        res.send(value);
-    })
-    .catch(error => {
-        console.log('rejected')
-        res.send(error)
-    })
+        .then((value) => {
+            console.log('resolved')
+            res.send(value);
+        })
+        .catch(error => {
+            console.log('rejected')
+            res.send(error)
+        })
 })
 
-router.get('/moveUp', (req,res) => {
+router.get('/moveUp', (req, res) => {
     moveUp()
-    .then((value) => {
-        console.log('resolved')
-        res.send(value);
-    })
-    .catch(error => {
-        console.log('rejected')
-        res.send(error)
-    })
+        .then((value) => {
+            console.log('resolved')
+            res.send(value);
+        })
+        .catch(error => {
+            console.log('rejected')
+            res.send(error)
+        })
 })
 
-router.get('/relaisOn', (req,res) => {
+router.get('/relaisOn', (req, res) => {
     relaisOn()
-    .then(() => {
-        res.send('relais on')
-    })
+        .then(() => {
+            res.send('relais on')
+        })
 })
 
-router.get('/relaisOff', (req,res) => {
+router.get('/relaisOff', (req, res) => {
     relaisOff()
-    .then(() => {
-        res.send('relais off')
-    })
+        .then(() => {
+            res.send('relais off')
+        })
 })
 
 export default router
